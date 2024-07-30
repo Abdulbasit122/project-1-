05d99f8882ad0d86806156ebb0b91a5c3bf34394
Abdul Basit Mujeeb <168018799+Abdulbasit122@users.noreply.github.com>
2024-07-30 09:57:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+    return true;
+}
+alert("thank you for your feedback");
+console.log("alert");
